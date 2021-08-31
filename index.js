7aced147f36450edbdfd7dccc6c46fb3db7506df
ellandis <ellandis@crimson.ua.edu>
2021-08-31 06:30:48 -0400

    Drumkit
    Event Listeners, objects and methods, keyboard event listeners, understanding callbacks

---
+++ index.js
@@ -0,0 +1,65 @@
+var numberOfButtons = document.querySelectorAll(".drum").length;
+
+for(var i = 0; i < numberOfButtons; i++){
+  document.querySelectorAll(".drum")[i].addEventListener("click",function (){
+    var buttonHTML = this.innerHTML;
+    makeSound(buttonHTML);
+    buttonAnimation(buttonHTML);
+
+  });
+}
+
+
+document.addEventListener('keydown',function(event){
+
+  makeSound(event.key);
+  buttonAnimation(event.key);
+
+});
+
+function makeSound(key){
+
+    var audio;
+    switch (key) {
+      case "w":
+        audio = new Audio("sounds/crash.mp3");
+        audio.play();
+        break;
+      case "a":
+          audio = new Audio("sounds/kick-bass.mp3");
+          audio.play();
+          break;
+      case "s":
+          audio = new Audio("sounds/snare.mp3");
+          audio.play();
+          break;
+      case "d":
+          audio = new Audio("sounds/tom-1.mp3");
+          audio.play();
+          break;
+      case "j":
+          audio = new Audio("sounds/tom-2.mp3");
+          audio.play();
+          break;
+      case "k":
+          audio = new Audio("sounds/tom-3.mp3");
+          audio.play();
+          break;
+      case "l":
+          audio = new Audio("sounds/tom-4.mp3  ");
+          audio.play();
+          break;
+
+      default:
+    }
+}
+
+function buttonAnimation(currentkey){
+
+  var activeButton = document.querySelector("." + currentkey);
+
+  activeButton.classList.add("pressed");
+
+  setTimeout(function(){
+    activeButton.classList.remove("pressed");}, 100);
+}
